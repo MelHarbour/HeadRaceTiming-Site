c29ee446666aa961a0edec71fed9f35cf91dee6c
Melvyn Harbour <melharbour@gmail.com>
2017-03-22 11:14:02 +0000

    More basic wireup

---
+++ HeadRaceTiming-Site/Controllers/CompetitionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HeadRaceTimingSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeadRaceTimingSite.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        private readonly TimingSiteContext _context;
+
+        public CompetitionController(TimingSiteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            Competition competition = await _context.Competitions
+                .SingleOrDefaultAsync(c => c.CompetitionId == id);
+            return View(competition);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ HeadRaceTiming-Site/Controllers/CrewApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HeadRaceTimingSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HeadRaceTimingSite.Controllers
+{
+    [Route("api/[controller]")]
+    public class CrewApiController : Controller
+    {
+        private readonly TimingSiteContext _context;
+
+        public CrewApiController(TimingSiteContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Crew>> GetAll()
+        {
+            return await _context.Crews.ToListAsync();
+        }
+    }
+}
