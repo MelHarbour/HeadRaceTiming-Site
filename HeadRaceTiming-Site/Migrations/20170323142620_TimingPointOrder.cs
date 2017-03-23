e587e2fa4ee3ba375c5f70edfb21f3bf04bf51c7
Melvyn Harbour <melharbour@gmail.com>
2017-03-23 14:27:10 +0000

    Add TimingPoint order

---
+++ HeadRaceTiming-Site/Migrations/20170323142620_TimingPointOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HeadRaceTimingSite.Migrations
+{
+    public partial class TimingPointOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "TimingPoints",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "TimingPoints");
+        }
+    }
+}
