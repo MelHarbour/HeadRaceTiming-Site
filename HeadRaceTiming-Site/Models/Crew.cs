07c9cf2a4b9133a218323a4817a6e0d6b69e15f1
Melvyn Harbour <melharbour@gmail.com>
2017-03-27 13:38:58 +0100

    Add method to calculate time

--- HeadRaceTiming-Site/Models/Crew.cs
+++ HeadRaceTiming-Site/Models/Crew.cs
@@ -12,6 +12,17 @@ namespace HeadRaceTimingSite.Models
         public string Name { get; set; }
         public int StartNumber { get; set; }
 
+        public TimeSpan? RunTime(TimingPoint startPoint, TimingPoint finishPoint)
+        {
+            Result start = Results.First(r => r.TimingPointId == startPoint.TimingPointId);
+            Result finish = Results.First(r => r.TimingPointId == finishPoint.TimingPointId);
+
+            if (start != null && finish != null)
+                return finish.TimeOfDay - start.TimeOfDay;
+            else
+                return null;
+        }
+
         public List<Result> Results { get; set; }
 
         public int CompetitionId { get; set; }
