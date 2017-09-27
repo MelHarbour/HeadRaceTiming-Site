cc002a6285a1067b572fecdbbae1f0f8349f500e
Melvyn Harbour <melharbour@gmail.com>
2017-09-27 13:41:21 +0100

    Refactoring

--- HeadRaceTiming-Site/Controllers/CrewApiController.cs
+++ HeadRaceTiming-Site/Controllers/CrewApiController.cs
@@ -21,19 +21,25 @@ namespace HeadRaceTimingSite.Controllers
             _context = context;
         }
 
-        [HttpGet("ByCompetition/{id}")]
-        public async Task<IEnumerable<ViewModels.Result>> GetByCompetition(int id)
+        private async Task<IEnumerable<Crew>> GetCrewList(int competitionId)
         {
-            IEnumerable<Crew> crews = await _context.Crews.Where(c => c.CompetitionId == id)
+            return await _context.Crews.Where(c => c.CompetitionId == competitionId)
                 .Include(x => x.Competition.TimingPoints).Include(x => x.Results)
                 .ToListAsync();
+        }
 
-            List<ViewModels.Result> results = crews.OrderBy(x => x.OverallTime).Select((x,i) => new ViewModels.Result()
-                    {
-                        CrewId = x.CrewId, Name = x.Name, StartNumber = x.StartNumber, OverallTime = x.OverallTime, Rank = (i+1).ToString(),
-                        FirstIntermediateTime = x.RunTime(x.Competition.TimingPoints[0].TimingPointId, x.Competition.TimingPoints[1].TimingPointId),
-                        SecondIntermediateTime = x.RunTime(x.Competition.TimingPoints[0].TimingPointId, x.Competition.TimingPoints[2].TimingPointId)
-                    })
+        private List<ViewModels.Result> BuildResultsList(IEnumerable<Crew> crews)
+        {
+            List<ViewModels.Result> results = crews.OrderBy(x => x.OverallTime).Select((x, i) => new ViewModels.Result()
+            {
+                CrewId = x.CrewId,
+                Name = x.Name,
+                StartNumber = x.StartNumber,
+                OverallTime = x.OverallTime,
+                Rank = (i + 1).ToString(),
+                FirstIntermediateTime = x.RunTime(x.Competition.TimingPoints[0].TimingPointId, x.Competition.TimingPoints[1].TimingPointId),
+                SecondIntermediateTime = x.RunTime(x.Competition.TimingPoints[0].TimingPointId, x.Competition.TimingPoints[2].TimingPointId)
+            })
                 .ToList();
 
             int firstRank = 1;
@@ -51,17 +57,28 @@ namespace HeadRaceTimingSite.Controllers
             return results;
         }
 
+        [HttpGet("{id}")]
+        public async Task<Crew> GetById(int id)
+        {
+            return await _context.Crews.FirstOrDefaultAsync(x => x.CrewId == id);
+        }
+
+        [HttpGet("ByCompetition/{id}")]
+        public async Task<IEnumerable<ViewModels.Result>> GetByCompetition(int id)
+        {
+            IEnumerable<Crew> crews = await GetCrewList(id);
+
+            return BuildResultsList(crews);
+        }
+
         [HttpGet("ByCompetition/{id}/{search}")]
         public async Task<IEnumerable<ViewModels.Result>> GetByCompetition(int id, string search)
         {
-            IEnumerable<Crew> crews = await _context.Crews.Where(c => c.CompetitionId == id)
-                .Include(x => x.Competition.TimingPoints).Include(x => x.Results)
-                .ToListAsync();
+            IEnumerable<Crew> crews = await GetCrewList(id);
+
             string lowerSearch = search.ToLower();
 
-            return crews.OrderBy(x => x.OverallTime).Select((x, i) => new ViewModels.Result() { CrewId = x.CrewId, Name = x.Name, StartNumber = x.StartNumber, OverallTime = x.OverallTime, Rank = (i + 1).ToString() })
-                .Where(x => x.Name.ToLower().Contains(lowerSearch))
-                .ToList();
+            return BuildResultsList(crews.Where(x => x.Name.ToLower().Contains(lowerSearch)));
         }
     }
 }
