5aa57d8e6cfc0c5ce444814c6a607f222fe80a57
Melvyn Harbour <melharbour@gmail.com>
2017-10-05 11:30:15 +0100

    More #29

---
+++ HeadRaceTiming-Site/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HeadRaceTimingSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Policy = "AdminsOnly")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
