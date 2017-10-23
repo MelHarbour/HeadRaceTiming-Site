c402a23f4bef1cf9de981558a3b654af39a29c42
Melvyn Harbour <melharbour@gmail.com>
2017-10-23 16:16:04 +0100

    Fix #44

---
+++ HeadRaceTiming-Site/Controllers/HelpController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeadRaceTimingSite.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
