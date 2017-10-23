e4efa620342c633908091f1d1a20c769d80dd546
Melvyn Harbour <melharbour@gmail.com>
2017-10-23 17:23:34 +0100

    Restructure for #9

--- HeadRaceTiming-Site/Controllers/AdminController.cs
+++ HeadRaceTiming-Site/Controllers/AdminController.cs
@@ -51,5 +51,30 @@ namespace HeadRaceTimingSite.Controllers
         {
             return View();
         }
+
+        /// <summary>
+        /// This class should match the CSV fields available in Competitors.csv (with spaces removed).
+        /// </summary>
+        private class CsvCrewAthlete
+        {
+            public int CrewID { get; set; }
+            public int Position { get; set; }
+            public string MembershipNumber { get; set; }
+            public string Surname { get; set; }
+            public string FirstName { get; set; }
+            public int Age { get; set; }
+            public DateTime DofB { get; set; }
+            public char Gender { get; set; }
+            public bool Cox { get; set; }
+            public string MembersClubName { get; set; }
+            public string MembersClubIndexCode { get; set; }
+            public int RowingPRI { get; set; }
+            public int ScullingPRI { get; set; }
+            public int RowingPRIMax { get; set; }
+            public int ScullingPRIMax { get; set; }
+            public int RowingPoints { get; set; }
+            public int ScullingPoints { get; set; }
+            public bool Substitute { get; set; }
+        }
     }
 }
