8f159d371a113940132947647db8b6df18301f9b
MelHarbour <melharbour@gmail.com>
2018-03-06 14:02:56 +0000

    Remove stray comment

--- HeadRaceTiming-Site/Controllers/CrewApiController.cs
+++ HeadRaceTiming-Site/Controllers/CrewApiController.cs
@@ -8,8 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using HeadRaceTimingSite.ViewModels;
 using System.Globalization;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace HeadRaceTimingSite.Controllers
 {
     [Route("api/[controller]")]
