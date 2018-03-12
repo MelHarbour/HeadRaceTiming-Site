d8d3c3d823916ed11c52ffba21c3572a82a4acce
MelHarbour <melharbour@gmail.com>
2018-03-12 17:05:18 +0000

    Package updates

--- HeadRaceTiming-Site/wwwroot/lib/paper-input/all-imports.d.ts
+++ HeadRaceTiming-Site/wwwroot/lib/paper-input/all-imports.d.ts
@@ -5,8 +5,9 @@
  *   https://github.com/Polymer/gen-typescript-declarations
  *
  * To modify these typings, edit the source file(s):
- *   test/index.html
+ *   all-imports.html
  */
 
-/// <reference path="../../web-component-tester/browser.d.ts" />
+/// <reference path="paper-input.d.ts" />
+/// <reference path="paper-textarea.d.ts" />
 
