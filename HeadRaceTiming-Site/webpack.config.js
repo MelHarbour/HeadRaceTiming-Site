4548959fde7ed9c10fa9ad5435e8c620d0026960
Melvyn Harbour <melvyn.harbour@red-gate.com>
2018-11-07 14:35:27 +0000

    Package update and move to webpack from gulp

---
+++ HeadRaceTiming-Site/webpack.config.js
@@ -0,0 +1,35 @@
+﻿const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+    entry: ['./wwwroot/src/site.js', './wwwroot/src/site.css'],
+    mode: 'production',
+    output: {
+        filename: 'site.js',
+        path: path.resolve(__dirname, 'wwwroot/dist')
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+}
