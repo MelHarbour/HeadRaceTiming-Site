152ff43149c4a1e3796b379048b0038ba237f927
Mel Harbour <melharbour@gmail.com>
2020-03-02 15:33:27 +0000

    Fix build process

--- HeadRaceTiming-Site/webpack.config.js
+++ HeadRaceTiming-Site/webpack.config.js
@@ -1,4 +1,4 @@
-/// <binding />
+/// <binding BeforeBuild='Run - Production' />
 const path = require('path');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
